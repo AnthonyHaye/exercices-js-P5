70aefb2a261042b6d5c2e6d1de0dca043706ecc1
Anthony Hayé <anthony.haye49@laposte.net>
2024-05-23 14:49:44 +0200

    exercice terminé

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,14 @@
 //votre code ici
+ function printNumbers(n) {
+    let result = '';
+    for (let i = 1; i <= n; i++) {
+        result += i + ' ';
+    }
+    return result.trim(); 
+}
+
+// Exemples d'utilisation
+console.log(printNumbers(5)); 
+console.log(printNumbers(10000000)); 
 
 export default printNumbers
--- Exercice8/script.js
+++ Exercice8/script.js
@@ -1,3 +1,7 @@
 //Votre code ici
 
+function add(nombre1, nombre2) {
+    return nombre1 + nombre2;
+} 
+
  export default add;
